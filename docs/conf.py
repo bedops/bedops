24ef6e565cf66285d8ba4d94f651fc28156b06b0
Alex Reynolds <alexpreynolds@d-69-91-184-235.dhcp4.washington.edu>
2013-08-28 13:39:33 -0700

    added logo with label

--- docs/conf.py
+++ docs/conf.py
@@ -113,7 +113,7 @@ html_theme = 'default'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'logo.png'
+html_logo = 'logo_with_label.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
