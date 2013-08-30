baed37fb59689f4dfb7dbf43f6c6e82a5a45c41c
Alex Reynolds <alexpreynolds@Papillion.local>
2013-08-30 01:44:13 -0700

    updated configuration

--- docs/conf.py
+++ docs/conf.py
@@ -99,7 +99,10 @@ html_theme = 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    "linkcolor": "blue",
+    "visitedlinkcolor": "darkblue"
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
