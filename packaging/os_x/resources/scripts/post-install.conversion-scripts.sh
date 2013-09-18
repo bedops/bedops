89713bd7e7b4603f5c059fbf482801c571898427
Alex Reynolds <alexpreynolds@Papillion.local>
2013-09-17 17:31:02 -0700

    removing unneeded OS X package components

--- packaging/os_x/resources/scripts/post-install.conversion-scripts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-install_name_tool -change /opt/local/lib/libstdc++.6.dylib /Library/Application\ Support/BEDOPS/libstdc++.6.dylib /usr/local/bin/wig2bed_bin
-install_name_tool -change /opt/local/lib/gcc47/libgcc_s.1.dylib /Library/Application\ Support/BEDOPS/libgcc_s.1.dylib /usr/local/bin/wig2bed_bin
-
-exit 0
-
-
