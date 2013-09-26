ea4398626e4ce56995761094bd74b94854991b2d
Alex Reynolds <alexpreynolds@Papillion.local>
2013-09-26 01:35:45 -0700

    updated GPL license data in source and headers

--- applications/bed/conversion/src/psl2starch.py
+++ applications/bed/conversion/src/psl2starch.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 
 #
+#    BEDOPS
+#    Copyright (C) 2011, 2012, 2013 Shane Neph, Scott Kuehn and Alex Reynolds
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+#
 # Author:       Alex Reynolds
 #
 # Project:      Converts 0-based, half-open [a-1,b) headered or headerless PSL input
--- applications/bed/sort-bed/src/Sort.cpp
+++ applications/bed/sort-bed/src/Sort.cpp
@@ -5,7 +5,24 @@
   ID: $Id: sort.c,v 1.6 2010/08/20 05:05:32 sjn Exp $
 */
 
-
+//
+//    BEDOPS
+//    Copyright (C) 2011, 2012, 2013 Shane Neph, Scott Kuehn and Alex Reynolds
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License along
+//    with this program; if not, write to the Free Software Foundation, Inc.,
+//    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
 
 #include <cctype>
 #include <cstdlib>
