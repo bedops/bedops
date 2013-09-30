3bbb2960887ceaf90fc1ad68c0044d729c98afd7
sjneph <sjneph@gmail.com>
2013-09-30 14:33:28 -0700

    added back in license information

--- applications/bed/bedmap/src/Bedmap.cpp
+++ applications/bed/bedmap/src/Bedmap.cpp
@@ -6,6 +6,25 @@
   ID: $Id$
 */
 
+//
+//    BEDOPS
+//    Copyright (C) 2011, 2012, 2013 Shane Neph, Scott Kuehn and Alex Reynolds
+//
+//    This program is free software; you can redistribute iterator and/or modify
+//    iterator under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that iterator will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License along
+//    with this program; if not, write to the Free Software Foundation, Inc.,
+//    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
 #include <cctype>
 #include <cstdlib>
 #include <exception>
