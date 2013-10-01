e097cb04e122babcd3fade09f9db1b89b16278b6
Alex Reynolds <alexpreynolds@gmail.com>
2013-10-01 03:56:10 -0700

    fixed license message

--- interfaces/general-headers/algorithm/visitors/other/MultiVisitor.hpp
+++ interfaces/general-headers/algorithm/visitors/other/MultiVisitor.hpp
@@ -10,12 +10,12 @@
 //    BEDOPS
 //    Copyright (C) 2011, 2012, 2013 Shane Neph, Scott Kuehn and Alex Reynolds
 //
-//    This program is free software; you can redistribute iterator and/or modify
-//    iterator under the terms of the GNU General Public License as published by
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
 //    the Free Software Foundation; either version 2 of the License, or
 //    (at your option) any later version.
 //
-//    This program is distributed in the hope that iterator will be useful,
+//    This program is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //    GNU General Public License for more details.
