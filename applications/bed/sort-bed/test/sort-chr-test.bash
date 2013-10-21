1d934767a32ff705daf212ca0471ddd86ea7a03b
Alex Reynolds <alexpreynolds@gmail.com>
2013-10-21 14:17:29 -0700

    testing more efficient script

--- applications/bed/sort-bed/test/sort-chr-test.bash
+++ applications/bed/sort-bed/test/sort-chr-test.bash
@@ -11,34 +11,22 @@
 # default) is different from the lexicographical sort out of sort-bed, this 
 # helps test how sort-bed handles BED data with large numbers of chromosomes.
 #
-# While it is inefficient to recreate a test file on each loop, this is done
-# to ensure an early exit does not leave a whole bunch of test files around.
-#
 
 CHROM_START=1
 CHROM_END=1024000
 
-for ((i = CHROM_START; i <= CHROM_END; i++))
+for ((idx = CHROM_START; idx <= CHROM_END; idx++))
 do
-    echo "testing sorting file with chrs ${CHROM_START}-${i}..." > /dev/stderr
-    for ((j = CHROM_START; j <= CHROM_END; j++))
-    do
-        if [ ${i} -ge ${j} ]
-        then
-            echo -e "chr${j}\t1\t2" >> .test_chr${i}.bed
-        else
-	    break
-	fi
-    done
-    ../bin/sort-bed --max-mem 2G .test_chr${i}.bed > /dev/null
+    echo "testing sorting file with chrs ${CHROM_START}-${idx}..." > /dev/stderr
+    echo -e "chr${idx}\t1\t2" >> .test.bed
+    ../bin/sort-bed --max-mem 2G .test.bed > /dev/null
     if [ $? -ne 0 ]
     then
         echo "failed on chr count [ ${i} ]"
         exit -1
-    else
-        rm .test_chr${i}.bed
     fi
 done
 
+rm .test.bed
 echo "success!"
 exit 0
