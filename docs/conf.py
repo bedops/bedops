cd152145aa5ecbc8678474914d0207d81ab462d8
Alex Reynolds <alexpreynolds@gmail.com>
2014-05-20 15:45:21 -0700

    updated v2p5p0 sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,8 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.mathjax', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode', 'sphinx.ext.mathbase']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.mathjax', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode'] 
+#, 'sphinx.ext.mathbase']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -127,7 +128,7 @@ html_favicon = 'assets/favicon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_build/html/_static']
+#html_static_path = ['_build/html/_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
@@ -315,4 +316,4 @@ intersphinx_mapping = {'http://docs.python.org/': None}
 
 RTD_OLD_THEME = True
 
-html_style = 'test.css'
+#html_style = 'test.css'
