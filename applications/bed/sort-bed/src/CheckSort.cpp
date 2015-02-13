8fd46cd4f7b5f176092581f209ed41e8f691db2b
Shane Neph <sjneph@gmail.com>
2015-02-13 15:10:14 -0800

    release --check-sort option for sort-bed

--- applications/bed/sort-bed/src/CheckSort.cpp
+++ applications/bed/sort-bed/src/CheckSort.cpp
@@ -1,3 +1,26 @@
+/*
+  Author: Shane Neph and Alex Reynolds
+    Date: Fri Feb 13 15:05:44 PST 2015
+*/
+//
+//    BEDOPS
+//    Copyright (C) 2011-2015 Shane Neph, Scott Kuehn and Alex Reynolds
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License along
+//    with this program; if not, write to the Free Software Foundation, Inc.,
+//    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
 #include <cstdio>
 #include <fstream>
 #include <string>
@@ -5,9 +28,7 @@
 #include "data/bed/BedCheckIterator.hpp"
 #include "data/bed/BedTypes.hpp"
 #include "utility/Exception.hpp"
-#include "utility/FPWrap.hpp"
 
-#include <iostream>
 int
 checkSort(char const **bedFileNames, unsigned int numFiles)
 {
@@ -18,7 +39,8 @@ checkSort(char const **bedFileNames, unsigned int numFiles)
       std::ifstream infile(bedFileNames[i]);
       if ( 0 != std::strcmp(bedFileNames[i], "-") && !infile )
         throw(Ext::UserError("Unable to find: " + std::string(bedFileNames[i])));
-      IterType start((std::strcmp(bedFileNames[i], "-") == 0) ? std::cin : infile, bedFileNames[i]), end, tmp;
+      IterType start((std::strcmp(bedFileNames[i], "-") == 0) ? std::cin : infile,
+                     bedFileNames[i]), end;
       while ( start != end )
         delete *start++;
     } // for each file
