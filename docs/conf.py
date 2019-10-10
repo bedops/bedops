70181348cbe5000dfa1a69847297c4a6bc79fdfe
Alex Reynolds <alexpreynolds@gmail.com>
2019-10-10 16:48:52 -0700

    updated conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -134,7 +134,7 @@ html_favicon = 'assets/favicon.ico'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = ['_build/html/_static']
-#html_static_path = ['_static']
+html_static_path = ['_static', 'assets']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
